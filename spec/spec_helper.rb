6088efe70896a8e6ec4bf5b3a7cf08b30595d208
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-05-06 16:39:22 +0200

    Rename filter for chefspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,6 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-ChefSpec::Coverage.start! { add_filter 'gotcms' }
+ChefSpec::Coverage.start! { add_filter 'example' }
 
 require 'chef/application'
