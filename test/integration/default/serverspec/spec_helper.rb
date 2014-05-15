6243a83c9fae4dbf23b3627b9a25bb482e108fac
Antoine Rouyer <antoine.rouyer@numergy.com>
2014-05-15 16:16:49 +0200

    Added junit formatter in spec_helper
    CD-47

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -2,14 +2,24 @@
 # coding: UTF-8
 # -*- coding: UTF-8 -*-
 #
+
 require 'serverspec'
 
 include SpecInfra::Helper::Exec
 include SpecInfra::Helper::DetectOS
+
+begin
+    require 'rspec_junit_formatter'
+rescue LoadError
+  require 'rubygems/dependency_installer'
+  Gem::DependencyInstaller.new.install('rspec_junit_formatter')
+  require 'rspec_junit_formatter'
+end
 
 RSpec.configure do |c|
   c.before :all do
     c.os = backend(Serverspec::Commands::Base).check_os
   end
   c.path = '/sbin:/usr/sbin:/usr/bin:/bin'
+  c.add_formatter 'RspecJunitFormatter', '/tmp/kitchen.xml'
 end
