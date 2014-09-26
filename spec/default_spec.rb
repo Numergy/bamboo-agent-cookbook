2480be89072a81a4d171c66872ed7e7766937185
Nicolas Blanc <sinfomicien@gmail.com>
2014-09-26 16:00:41 +0200

    Add unit test on both debian and centos, and use cached feature as no mock is done

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,12 +2,33 @@
 #
 require_relative 'spec_helper'
 
-describe 'bamboo-agent::default' do
-  subject { ChefSpec::Runner.new.converge(described_recipe) }
+describe 'debian=>bamboo-agent::default' do
+  # speedup thing as you don't use mocking
+  cached(:subject) do
+    runner = ChefSpec::Runner.new(platform: 'debian', version: '7.4')
+    runner.converge('bamboo-agent::default')
+  end
 
   # Write full examples using the `expect` syntax
   it 'does includes recipes' do
     expect(subject).to include_recipe('apt')
+    expect(subject).to include_recipe('augeas::geminstall')
+    expect(subject).to include_recipe('java')
+    expect(subject).to include_recipe('bamboo-agent::download')
+    expect(subject).to include_recipe('bamboo-agent::install')
+  end
+end
+
+describe 'centos=>bamboo-agent::default' do
+  # speedup thing as you don't use mocking
+  cached(:subject) do
+    runner = ChefSpec::Runner.new(platform: 'centos', version: '6.0')
+    runner.converge('bamboo-agent::default')
+  end
+
+  # Write full examples using the `expect` syntax
+  it 'does includes recipes' do
+    expect(subject).to include_recipe('yum')
     expect(subject).to include_recipe('augeas::geminstall')
     expect(subject).to include_recipe('java')
     expect(subject).to include_recipe('bamboo-agent::download')
