0cc81d82ec58c0c2d93380e9c792990be4cebdf7
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2014-09-29 18:44:53 +0200

    Ubuntu 13.04 is no longer supported and use apt attributes to run update at compile time.

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,10 +8,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
+node.default['apt']['compile_time_update'] = true
 case node['platform']
 when 'debian', 'ubuntu'
   include_recipe 'apt'
-  resources(execute: 'apt-get-update').run_action(:run)
 when 'redhat', 'centos', 'fedora'
   include_recipe 'yum'
 end
