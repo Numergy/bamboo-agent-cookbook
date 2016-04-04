050eb28fdb87f79e281fd8ade6f5aabf12cc1efe
Eric Williams <eric@subcritical.org>
2016-04-04 10:34:28 +0100

    Ignore use_inline_resources in foodcritic FC059

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license 'Apache 2.0'
 description 'Installs/Configures a bamboo agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.7.1'
+issues_url 'https://github.com/Numergy/bamboo-agent-cookbook/issues'
+source_url 'https://github.com/Numergy/bamboo-agent-cookbook'
 
 supports 'ubuntu'
 supports 'debian', '>= 7.0'
